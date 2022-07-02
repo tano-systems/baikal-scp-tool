edb9e793263d9e37538ffeb3a1884c815001b427
Anton Kikin <a.kikin@tano-systems.com>
2022-07-02 13:26:02 +0300

    build: Improve kernel release and arch detection
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- cmake/FindKernelHeaders.cmake
+++ cmake/FindKernelHeaders.cmake
@@ -1,30 +1,40 @@
-﻿# Find the kernel release
-execute_process(
-	COMMAND uname -r
-	OUTPUT_VARIABLE KERNEL_RELEASE
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+﻿# Default x86 kernel arch
+if(NOT KERNEL_ARCH)
+	set(KERNEL_ARCH amd64)
+endif()
+
+# Find the kernel release
+if(NOT KERNEL_VERSION)
+	execute_process(
+		COMMAND uname -r
+		OUTPUT_VARIABLE KERNEL_RELEASE
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+else(NOT KERNEL_VERSION)
+	set(KERNEL_RELEASE ${KERNEL_VERSION})
+endif()
 
 # Find the headers
 find_path(KERNELHEADERS_DIR
 	include/linux/user.h
 	PATHS
 		/usr/src/kernels/${KERNEL_RELEASE}
+		/usr/src/linux-headers-${KERNEL_RELEASE}-${KERNEL_ARCH}
 		/usr/src/linux-headers-${KERNEL_RELEASE}
+		/usr/src/linux-headers-${KERNEL_RELEASE}-common
 )
 
+# Find the sources
+find_path(KERNELSOURCES_DIR
+	include/generated/autoconf.h
+	PATHS
+		/usr/src/kernels/${KERNEL_RELEASE}
+		/usr/src/linux-headers-${KERNEL_RELEASE}-${KERNEL_ARCH}
+		/usr/src/linux-headers-${KERNEL_RELEASE}
+		/usr/src/linux-headers-${KERNEL_RELEASE}-common
+)
+
+message(STATUS "Kernel arch: ${KERNEL_ARCH}")
 message(STATUS "Kernel release: ${KERNEL_RELEASE}")
 message(STATUS "Kernel headers: ${KERNELHEADERS_DIR}")
-
-if (KERNELHEADERS_DIR)
-	set(KERNELHEADERS_INCLUDE_DIRS
-		${KERNELHEADERS_DIR}/include
-		${KERNELHEADERS_DIR}/arch/x86/include
-		CACHE PATH "Kernel headers include dirs"
-	)
-	set(KERNELHEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
-else (KERNELHEADERS_DIR)
-	set(KERNELHEADERS_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
-endif (KERNELHEADERS_DIR)
-
-mark_as_advanced(KERNELHEADERS_FOUND)
+message(STATUS "Kernel sources: ${KERNELSOURCES_DIR}")
